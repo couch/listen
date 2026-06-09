01f0d714bc2a8aac944a61d4ecfe510e33566211
couch <couch@users.noreply.github.com>
2026-06-09 14:55:51 +0000

    deploy: f2e9941b5c1973e0dc5b770d99d6478dc88c0489

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE = 'muxtape-205effc4';
+const CACHE = 'muxtape-3cebebef';
 
 // Resources to pre-cache on install (the HTML shell + bundled assets)
-const PRECACHE = ["/","/config.js","/assets/admin-BvE0ni-8.css","/assets/main-B7eDKgZL.css","/assets/admin-TgbU8d2S.js","/assets/main-C_onrjTz.js","/assets/utils-C5o64KIB.js","/admin.html","/embed.html"];
+const PRECACHE = ["/","/config.js","/assets/admin-BvE0ni-8.css","/assets/main-B7eDKgZL.css","/assets/admin-TgbU8d2S.js","/assets/main-CprxDDV4.js","/assets/utils-C5o64KIB.js","/admin.html","/embed.html"];
 
 self.addEventListener('install', event => {
   event.waitUntil(
